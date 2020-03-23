105fdd085349c1557b8c8c0f599ef121730f2945
fuchb <15842635958@163.com>
2020-03-24 02:21:09 +0900

    no message

--- myTest/src/components/CameraTest.tsx
+++ myTest/src/components/CameraTest.tsx
@@ -1,5 +1,5 @@
 import { Component, ReactNode, createElement } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, CameraRoll } from "react-native";
 import {RNCamera} from 'react-native-camera';
 import { styles } from "../ui/styles";
 
@@ -17,9 +17,6 @@ export class CameraTest extends Component {
                     type={RNCamera.Constants.Type.back}>
                    <View>
                         <Text style={styles.button} onPress={this.takePicture.bind(this)}>[take photo]</Text>
-                        <Image style={{width: 100, height: 100, marginBottom: 20}} source={{uri: super.state.imagePath}}>
-
-                        </Image>
                     </View> 
                     </RNCamera>
             </View>
@@ -28,9 +25,7 @@ export class CameraTest extends Component {
     private async takePicture():  Promise<number> {
         var options: any = { quality: 0.5, base64: true };
         const data = await this.camera.takePictureAsync(options);
-        this.setState({
-            imagePath: data.uri,
-        });
+        CameraRoll.saveToCameraRoll(data.uri);
         console.log(data.uri);
         return 0;
     }
