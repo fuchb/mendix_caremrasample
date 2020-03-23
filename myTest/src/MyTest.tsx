3a5fc5dd756b21cdb5915c25ad397ebaad767e3b
fuchb <15842635958@163.com>
2020-03-23 13:42:24 +0900

    no message

--- myTest/src/MyTest.tsx
+++ myTest/src/MyTest.tsx
@@ -1,59 +1,15 @@
+// import { hot } from "react-hot-loader/root";
+import { CameraTest } from "./components/CameraTest";
 import { Component, ReactNode, createElement } from "react";
-import { View, Text } from "react-native";
-import {RNCamera} from 'react-native-camera';
+
 
 export class MyTest extends Component {
 
-   /* constructor(props) {
-        super(props);
-        this.state = {
-            moveAnim: new Animated.Value(0)
-        };
-    }
-
-    componentDidMount() {
-        this.startAnimation();
-    }
-
-    startAnimation = () => {
-        this.state.moveAnim.setValue(0);
-        Animated.timing(
-            this.state.moveAnim,
-            {
-                toValue: -200,
-                duration: 1500,
-                easing: Easing.linear
-            }
-        ).start(() => this.startAnimation());
-    };
-    //  识别二维码
-    onBarCodeRead = (result) => {
-        const { navigate } = props.navigation;
-               const {data} = result;
-            navigate('Sale', {
-                url: data
-            })
-    };*/
-
     render():ReactNode {
         return (
-            <View>
-                <RNCamera
-				    style={{width: 600,height: 555}}
-                    /* ref={ref => {
-                        camera = ref;
-                    }}*/
-                    type={RNCamera.Constants.Type.back}
-                    /*flashMode={RNCamera.Constants.FlashMode.on}
-                    onBarCodeRead={onBarCodeRead}*/
-                >
-                   <View>
-                        <View /*style={styles.rectangle}*//>
-                        <Text></Text>
-                    </View> 
-                    </RNCamera>
-            </View>
+            <CameraTest/>
         );
     }
 
 }
+// export default hot(CameraTest);// CameraTestを変更すると、該当部品をもう一度ビルドする。
--- myTest/src/ui/styles.ts
+++ myTest/src/ui/styles.ts
@@ -4,6 +4,33 @@ export const styles: any = StyleSheet.create({
     flex: {
         flexDirection: "row"
     },
+	container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+	capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    },
+	button: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40,
+  },
+	rectangle: {
+        height: 400,
+        width: 400,
+        borderWidth: 1,
+        borderColor: '#00FF00',
+        backgroundColor: 'transparent'
+    },
     badge: {
         borderRadius: 30,
         paddingLeft: 10,
@@ -11,6 +38,11 @@ export const styles: any = StyleSheet.create({
         paddingTop: 5,
         paddingBottom: 5,
         backgroundColor: "#D9534F"
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     },
     label: {
         textAlign: "center",
